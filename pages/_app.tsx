0cd336d52b3680ceef16d5bb22d7114f7c74e5d3
zhuocun <zhuocun.tang@outlook.com>
2022-11-06 03:24:41 +1100

    refactor: close persistor

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,19 @@
 import React from "react";
 import "../styles/globals.css";
 import "../i18n/configs";
-import rootStore from "../redux/store";
+import store from "../redux/store";
 import { Provider } from "react-redux";
 import axios from "axios";
-import { PersistGate } from "redux-persist/integration/react";
 import { AppProps } from "next/app";
 
 axios.defaults.headers["x-icode"] = "D79AA31B83415E95";
 
 const NextJSTrip = ({ Component, pageProps }: AppProps) => {
     return (
-        <Provider store={rootStore.store}>
-            <PersistGate persistor={rootStore.persistor} loading={null}>
-                <Component {...pageProps} />
-            </PersistGate>
+        <Provider store={store}>
+            {/*<PersistGate persistor={rootStore.persistor} loading={null}>*/}
+            <Component {...pageProps} />
+            {/*</PersistGate>*/}
         </Provider>
     );
 };
--- redux/store.ts
+++ redux/store.ts
@@ -6,14 +6,14 @@ import { authSlice } from "./reducers/authSlice";
 import { cartSlice } from "./reducers/cartSlice";
 import { orderSlice } from "./reducers/orderSlice";
 import { languageSlice } from "./reducers/langSlice";
-import { persistReducer, persistStore } from "redux-persist";
-import storage from "redux-persist/lib/storage";
+// import { persistReducer, persistStore } from "redux-persist";
+// import storage from "redux-persist/lib/storage";
 
-const persistConfig = {
-    key: "root",
-    storage,
-    whiteList: ["user"]
-};
+// const persistConfig = {
+//     key: "root",
+//     storage,
+//     whiteList: ["user"]
+// };
 
 const rootReducer = combineReducers({
     language: languageSlice.reducer,
@@ -25,21 +25,23 @@ const rootReducer = combineReducers({
     order: orderSlice.reducer
 });
 
-const persistedReducer = persistReducer(persistConfig, rootReducer);
+// const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 // reducers are saved in store
 const store = configureStore({
-    reducer: persistedReducer,
+    reducer: rootReducer,
     // middleware: (getDefaultMiddleware) =>
     //     getDefaultMiddleware().concat(actionLog),
     devTools: true
 });
 
-const persistor = persistStore(store);
+// const persistor = persistStore(store);
 
 // state of the store, including everything in redux folder
 export type RootState = ReturnType<typeof store.getState>;
 export type ReduxDispatch = typeof store.dispatch;
 
-const stores = { store, persistor };
-export default stores;
+// const stores = { store, persistor };
+// export default stores;
+
+export default store;
